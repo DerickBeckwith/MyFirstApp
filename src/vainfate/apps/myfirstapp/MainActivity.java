8446678fe0ae79d498c702ab8597b755b46c0753
Derick Beckwith <derick.beckwith@gmail.com>
2014-01-27 17:12:17 -0500

    Added code to handle clicking the Send button

--- src/vainfate/apps/myfirstapp/MainActivity.java
+++ src/vainfate/apps/myfirstapp/MainActivity.java
@@ -3,6 +3,7 @@ package vainfate.apps.myfirstapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +18,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	// Called when the user clicks the Send button
+	public void sendMessage(View view) {
+		
 	}
 
 }
